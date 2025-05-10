56808bb27e21311bcf44578c72c6a454b1e0605d
xxhusa <andre.rocha74@etec.sp.gov.br>
2025-05-10 09:50:56 -0300

    projetoexcompleto

--- projeto.c
+++ projeto.c
@@ -14,7 +14,9 @@ int main (){
 	float media;
 	float cel;
 	float fah;
-	
+	char nome [20];
+	float dolar, real, taxa;
+		
 	
 	
 	
@@ -25,20 +27,25 @@ int main (){
         //printf("Resolucao de equacao de segundo grau\n");
 		printf("Menu:\n");
         printf("1. Resolver equacao\n");
-        printf("2. Diga Ola Mundo\n");
-        printf("3. Soma\n");
-        printf("4. calcule a media de nota\n");
-        printf("5. Conversao de temperatura celsius para fahrenheit\n");
-        printf("6. Sair\n");
+        printf("2. Qual seu nome\n");
+        printf("3. Diga Ola Mundo\n");
+        printf("4. Soma\n");
+        printf("5. calcule a media de nota\n");
+        printf("6. Conversao de temperatura celsius para fahrenheit\n");
+        printf("7. Conversao dolar a real\n");
+        printf("8. Sair\n");
         printf("Escolha uma opcao: ");
         scanf(" %d", &n); // Lê a opção do usuário,
         
         system("cls || clear");
         
-        if(n == 6) {
+        if(n == 8) {
+        	
         	printf("programa encerrado.\n");
         	break;
+        	
         } else if (n == 1) { 
+        
         //printf("Voltando ao Menu\n");
         printf("coloque os valores de a, b e c:\n");
 	scanf("%f %f %f", &a, &b, &c); // scanf pede para mostrarmos quais numeros vao ser incluidos nas variveis
@@ -53,19 +60,25 @@ int main (){
 		x1 = (-b + sqrt(delta)) / (2 * a);
 		x2 = (-b - sqrt(delta)) / (2 * a);
 		printf("As raizes sao: %.2f e %.2f\n", x1, x2);
+		
 	} 
 	printf("\nVoltando ao Inicio\n");
         continue;
-		} else if(n == 2){
+		} else if(n == 3){
+			
 			printf("Hello Mundo\n");
-		} else if (n == 3){
+			
+		} else if (n == 4){
+			
 			printf("Digite um numero:\n");
 	scanf("%d",&numero1);
 	printf("Digite outro numero:\n");
 	scanf("%d",&numero2);
 	resultado = numero1+numero2;
 	printf("O resultado:%d\n",resultado);
-		} else if(n == 4){
+	
+		} else if(n == 5){
+			
 			printf("insira a nota 1:\n");
 	scanf("%f",&nota1);
 	printf("insira a nota 2:\n");
@@ -78,12 +91,30 @@ int main (){
 		printf("Aprovado");}
 	else{
 		printf("Reprovado");}
-		} else if(n == 5){
+		
+		} else if(n == 6){
+			
 			printf("Digite a temperatura em graus celsius:\n");
 	scanf("%f",&cel);
 	fah = cel * 9/5 + 32;
 	printf("A temperatura em fahrenheit sera: %f\n", fah);
 		
+		}else if (n == 2){
+			
+			printf("Qual seu nome:\n");
+			scanf("%s",&nome);
+			printf("Ola %s",nome);
+			
+		}else if (n == 7){
+			
+			printf("Quanto voce tem em dolar:\n");
+	scanf("%f",&dolar);
+	printf("qual o valor do dolar em real:\n");
+	scanf("%f",&taxa);
+	real = (dolar * taxa);
+	printf("voce tem entao em real:%.2f\n", real);
+			
+			
 		}
 		printf("\nVoltando ao Menu\n");
         continue;
